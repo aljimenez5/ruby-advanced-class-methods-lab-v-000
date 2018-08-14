47739c3d805c8ca9f07305ebba287c3b084df90e
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-14 06:32:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,14 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
+
+  def self.create
+    song = self.new
+    song.save
+    return song
+  end
 
   def self.all
     @@all
@@ -9,5 +17,43 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    return song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    return song
+  end
+
+  def self.find_by_name(name)
+    @@all.detect {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    filename.split('-','.').delete(mp3)
+    binding.pry
+  end
+
+  def self.create_from_filename
+    self.new_from_filename
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
 
 end
